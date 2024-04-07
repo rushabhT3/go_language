7bddabc1d9d433c12f0054ebc9add631dfb1c3bb
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2024-04-07 08:50:59 +0000

    post is not working [4977f217483a2e047e4d8c462c16f3c7ec392b61]

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"log"
 	"math/rand"
 	middlewares "my_gin_project/middlewares" // import your middleware package as middlewares
 	"net/http"
@@ -52,46 +53,54 @@ func main() {
     })
 
     router.POST("/external", func(c *gin.Context) {
-        // Generate a random number
-        randomNum := rand.Intn(100) // generates a random integer between 0 and 100
+    // Generate a random number
+    randomNum := rand.Intn(100) // generates a random integer between 0 and 100
+    log.Println("Generated random number:", randomNum)
 
-        // Create a map with the data you want to send
-        data := map[string]string{
-            "randomKey":   "key" + strconv.Itoa(randomNum), // generates a random key
-            "randomValue": "value" + strconv.Itoa(randomNum), // generates a random value
-        }
+    // Create a map with the data you want to send
+    data := map[string]string{
+        "randomKey":   "key" + strconv.Itoa(randomNum), // generates a random key
+        "randomValue": "value" + strconv.Itoa(randomNum), // generates a random value
+    }
+    log.Println("Created data map:", data)
 
-        // Marshal the map to JSON
-        jsonData, err := json.Marshal(data)
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "error": "Failed to marshal data to JSON",
-            })
-            return
-        }
+    // Marshal the map to JSON
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+        log.Println("Failed to marshal data to JSON:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{
+            "error": "Failed to marshal data to JSON",
+        })
+        return
+    }
+    log.Println("Marshalled data to JSON:", string(jsonData))
 
-        // Make a POST request to the external API
-        resp, err := http.Post("https://crudcrud.com/api/d7b51435183d4369aeac85a3c2218642", "application/json", bytes.NewBuffer(jsonData))
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "error": "Failed to make POST request",
-            })
-            return
-        }
-        defer resp.Body.Close()
+    // Make a POST request to the external API
+    resp, err := http.Post("https://crudcrud.com/api/d7b51435183d4369aeac85a3c2218642", "application/json", bytes.NewBuffer(jsonData))
+    if err != nil {
+        log.Println("Failed to make POST request:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{
+            "error": "Failed to make POST request",
+        })
+        return
+    }
+    log.Println("Made POST request, response status:", resp.Status)
+    defer resp.Body.Close()
 
-        // Read the response body
-        body, err := io.ReadAll(resp.Body)
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{
-                "error": "Failed to read response body",
-            })
-            return
-        }
+    // Read the response body
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        log.Println("Failed to read response body:", err)
+        c.JSON(http.StatusInternalServerError, gin.H{
+            "error": "Failed to read response body",
+        })
+        return
+    }
+    log.Println("Read response body:", string(body))
 
-        // Send the response body as the response
-        c.String(http.StatusOK, string(body))
-    })
-    
+    // Send the response body as the response
+    c.String(http.StatusOK, string(body))
+})
+
     router.Run(":8080")
 }
